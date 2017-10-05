00efd2c0c412aa6db0059bb9f4d83fb0147baf8b
Mike Shade <mshade@forumone.com>
2017-10-05 12:16:09 -0400

    fix path in pre-provision from puppet to config

--- web-starter/templates/config/shell/pre-provision.sh
+++ web-starter/templates/config/shell/pre-provision.sh
@@ -2,7 +2,7 @@
 
 VAGRANT_CORE_FOLDER="/vagrant"
 
-if [[ -f "${VAGRANT_CORE_FOLDER}/puppet/shell/custom/pre-provision.sh" ]]; then
-  source ${VAGRANT_CORE_FOLDER}/puppet/shell/custom/pre-provision.sh
+if [[ -f "${VAGRANT_CORE_FOLDER}/config/shell/custom/pre-provision.sh" ]]; then
+  source ${VAGRANT_CORE_FOLDER}/config/shell/custom/pre-provision.sh
 fi;
 
